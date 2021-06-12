b4ae850219955c3bd78cf550dc32f4587d0339f0
root <root@docker0.eb.localdomain>
2021-06-12 13:03:19 +0200

    added mkdir & readme

---
+++ README.md
@@ -0,0 +1,2 @@
+
+just a few systemd files to setup paperlessng via podman.
--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 podman pod create -n paperlessngpod  -p 8001:8000
 
+mkdir -p /srv/paperlessng_postgresql  /srv/paperlessng_consume /srv/paperlessng_data /srv/paperlessng_export /srv/paperlessng_media
 
 cp podman*service /etc/systemd/system/
 systemctl daemon-reload
